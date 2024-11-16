5cb61c7201f5e38896f6d221641baa2fd49abe83
Peter Phan <cryptogameking@protonmail.com>
2024-11-15 18:21:01 -0800

    Finished Hour 14
    Hour 14 is done.

---
+++ pphan_Hour14and16/Assets/Hour 14 Package/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+	public string sceneToLoad = "game_Description";
+
+
+	public void LoadGame ()
+	{
+		SceneManager.LoadScene(sceneToLoad);
+	}
+}
---
+++ pphan_Hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,18 @@
+/*
+
+HOUR 14 MODIFICATIONS OF THE EXERCISE:
+
+1. The background image of the panel has been changed from the "clouds.jpg" to the "BG_DizzyTreasure.png."
+
+2. The title text has been replaced by a custom image with transparent background "Dizzy Treasure."
+
+3. The subtitle's text color has been changed from default color of white to a golden color;
+    its size has been changed to 45, and it also has been bolden.
+
+4. The "Start Game" button has been enlarged with the changes of its text size increased to 45 and color to Red;
+    Also, the button's "Highligted Color" has been changed to Yellow, and its "Pressed Color" has been changed to Green.
+
+5. A scene called "game_Description" has been created with similar elements of "pphan_Hour14" scene with an exception of
+    the rainbow text; and it has been integrated between the starting screen (pphan_Hour14 scene) and the empty game scene.
+
+*/
