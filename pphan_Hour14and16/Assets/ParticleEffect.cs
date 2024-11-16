93384e813e64a09829ca987de054b621005fb0c9
Peter Phan <cryptogameking@protonmail.com>
2024-11-15 23:36:42 -0800

    Finished Hour 14 and 16
    Both Hours of 14 and 16 are done.

---
+++ pphan_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,21 @@
+/*
+
+HOUR 16 MODIFY 5 OR MORE DIFFERENT PARAMETERS:
+
+1. Under the default module of the particle system, the duration has been changed from 5 to 8 seconds;
+    also, its color has been changed from White to Gold.
+
+2. Under the Emission module of the particle system, the Rate over Time has been changed from 10 to 88;
+    also, its setting of Constant has been changed to Curve with a new key at highest midpoint of 88;
+    the start and end are set at zero. Bursts have been added with the time of 0.08 and the count of 888
+    along with other default settings.
+
+3. Under the Shape module of the particle system, the Angle has been changed from 25 to 35 degrees.
+
+4. Under the Color over Lifetime module of the particle system, its color has been changed from white to Red.
+
+5. Under the Size over Lifetime module of the particle system, all axis change with the same rate.
+
+6. Under the Lights module of the particle system, a customized Spot Light was added.
+
+ */
